9037abd26286cd3663355e4f403b45ef193449dd
Waseem <bsf1801678@ue.edu.pk>
2023-10-06 23:23:12 +0500

    10-6

--- server.js
+++ server.js
@@ -5,25 +5,30 @@ const app = express();
 // for readonly route/path
 app.get("/student", async (req, res) => {
   try {
-    const students = await sendQuery("select name, rollNo, contact from student");
-    res.send({ students: students});
+    const students = await sendQuery(
+      "select name, rollNo, contact from student"
+    );
+    res.send({ students });
   } catch (error) {
     res.send("An error occured");
   }
 });
 
-app.get("/departments", async(req, res) => {
+app.get("/departments", async (req, res) => {
   try {
     const departments = await sendQuery("select id, name from departments");
     console.log(departments);
     const result = [];
     for await (const department of departments) {
-      const programs = await sendQuery("select id, name, shifts from programs p where p.fk_department = " + department.id);
+      const programs = await sendQuery(
+        "select id, name, shifts from programs p where p.fk_department = " +
+          department.id
+      );
       if (programs.length > 0) {
         result.push({
           ...department,
-          programs
-        })
+          programs,
+        });
       }
     }
     res.send(result);
@@ -34,10 +39,27 @@ app.get("/departments", async(req, res) => {
     res.send("An error occured");
   }
 });
-app.get("/programs", async(req, res) => {
+app.get("/programs", async (req, res) => {
   try {
-    const programs = await sendQuery("select * from departments");
-    res.send({ programs: programs});
+    const programs = await sendQuery(`select id, name from programs`);
+
+    const myResult = [];
+    for await (const program of programs) {
+      const students = await sendQuery(`select
+       	s.*
+       from
+       	student s
+       inner join programs_students ps on ps.fk_student = s.id
+       where ps.fk_program = ${program.id}
+       `);
+      if (students.length > 0) {
+        myResult.push({
+          ...program,
+          students,
+        });
+      }
+    }
+    res.send({ programs: myResult });
   } catch (error) {
     res.send("An error occured");
   }
